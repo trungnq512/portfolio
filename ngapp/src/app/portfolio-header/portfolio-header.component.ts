39fa3761e18c4ec8c1e6fd433fb3cf524a3a86b7
trungnq512 <trungnq512.it@gmail.com>
2025-03-17 15:47:07 +0700

    refactor header

--- ngapp/src/app/portfolio-header/portfolio-header.component.ts
+++ ngapp/src/app/portfolio-header/portfolio-header.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { HeaderComponent } from './header/header.component';
-import { TopStripComponent } from './top-strip/top-strip.component';
+import { CommonAngularModule } from '../common-angular.module';
+import { MaterialModule } from '../material.module';
 
 @Component({
   selector: 'app-portfolio-header',
   templateUrl: './portfolio-header.component.html',
   styleUrl: './portfolio-header.component.css',
-  imports: [HeaderComponent, TopStripComponent]
+  imports: [MaterialModule,
+      CommonAngularModule]
 })
 export class PortfolioHeaderComponent {
 
