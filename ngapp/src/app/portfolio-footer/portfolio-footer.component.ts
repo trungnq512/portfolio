41027274da7e6c4e827aeecf753cd0d7887dec06
trungnq512 <trungnq512.it@gmail.com>
2025-03-18 09:36:38 +0700

    Added footer

--- ngapp/src/app/portfolio-footer/portfolio-footer.component.ts
+++ ngapp/src/app/portfolio-footer/portfolio-footer.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonAngularModule } from '../common-angular.module';
+import { MaterialModule } from '../material.module';
 
 @Component({
   selector: 'app-portfolio-footer',
   templateUrl: './portfolio-footer.component.html',
-  styleUrl: './portfolio-footer.component.css'
+  styleUrl: './portfolio-footer.component.css',
+  imports: [MaterialModule,
+        CommonAngularModule]
 })
 export class PortfolioFooterComponent {
 
